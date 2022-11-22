7bfded7d813716a8ffbedf0372f984e710238321
Neža Vižintin <neza.vizintin@yahoo.com>
2022-11-22 19:01:31 +0100

    started building on main.py and hopefully fixed issues with .idea not being ignored by git

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import json
+
 from flask import Flask, render_template
+from requests_oauthlib import OAuth2Session
+import os
+
+try:
+    import secrets # only needed for localhost, that's why it's in the try/except statement
+except ImportError as e:
+    pass
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/github/login")
+def github_login():
+    
+
 
 @app.route("/profile")
 def profile():
